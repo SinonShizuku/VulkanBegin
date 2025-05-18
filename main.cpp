779a09e1ede23ff7b8c42ca1c76eb96578fff421
SinonShizuku <3058725293@qq.com>
2025-05-18 17:40:12 +0800

    try dynamic rendering

--- main.cpp
+++ main.cpp
@@ -59,13 +59,38 @@ void CreatePipeline() {
             frag.StageCreateInfo(VK_SHADER_STAGE_FRAGMENT_BIT)
     };
     auto Create = [] {
+        VkPipelineRenderingCreateInfoKHR pipelineRenderingCreateInfo = {
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO,
+            .colorAttachmentCount = 1,
+            .pColorAttachmentFormats = &graphicsBase::Base().SwapchainCreateInfo().imageFormat
+        };
+
         graphicsPipelineCreateInfoPack pipelineCiPack;
+        pipelineCiPack.createInfo.pNext = &pipelineRenderingCreateInfo;
         pipelineCiPack.createInfo.layout = pipelineLayout_texture;
-        pipelineCiPack.createInfo.renderPass = RenderPassAndFramebuffers().renderPass;
-        pipelineCiPack.vertexInputBindings.emplace_back(0, sizeof(vertex), VK_VERTEX_INPUT_RATE_VERTEX);
-        pipelineCiPack.vertexInputAttributes.emplace_back(0, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(vertex, position));
-        pipelineCiPack.vertexInputAttributes.emplace_back(1, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(vertex, texCoord));
-        pipelineCiPack.inputAssemblyStateCi.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP;
+
+        // 设置顶点输入绑定
+        pipelineCiPack.vertexInputBindings.push_back({
+            .binding = 0,
+            .stride = sizeof(vertex),
+            .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+        });
+
+        // 设置顶点属性
+        pipelineCiPack.vertexInputAttributes.push_back({
+            .location = 0,
+            .binding = 0,
+            .format = VK_FORMAT_R32G32_SFLOAT,
+            .offset = offsetof(vertex, position)
+        });
+        pipelineCiPack.vertexInputAttributes.push_back({
+            .location = 1,
+            .binding = 0,
+            .format = VK_FORMAT_R32G32_SFLOAT,
+            .offset = offsetof(vertex, texCoord)
+        });
+
+        pipelineCiPack.inputAssemblyStateCi.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
         pipelineCiPack.viewports.emplace_back(0.f, 0.f, float(windowSize.width), float(windowSize.height), 0.f, 1.f);
         pipelineCiPack.scissors.emplace_back(VkOffset2D{}, windowSize);
         pipelineCiPack.multisampleStateCi.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
@@ -85,11 +110,34 @@ void CreatePipeline() {
 
 
 int main() {
-    // 初始化窗口
-    if (!InitializeWindow({1920,1080})) {
-        std::cerr<<"Fatal:Initialize Window failed..."<<std::endl;
+    //考虑1.2&1.3的兼容
+    PFN_vkCmdBeginRenderingKHR vkCmdBeginRendering = ::vkCmdBeginRendering;
+    PFN_vkCmdEndRenderingKHR vkCmdEndRendering = ::vkCmdEndRendering;
+
+    graphicsBase::Base().UseLatestApiVersion();
+    if (graphicsBase::Base().ApiVersion() < VK_API_VERSION_1_2) {
+        std::cerr << "Fatal:Vulkan API Version should be higher than 1.2 ..." << std::endl;
         return -1;
     }
+    if (graphicsBase::Base().ApiVersion() < VK_API_VERSION_1_3) {
+        graphicsBase::Base().AddDeviceExtension(VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME);
+        VkPhysicalDeviceDynamicRenderingFeatures physicalDeviceDynamicRenderingFeatures = {
+            VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DYNAMIC_RENDERING_FEATURES,
+        };
+        graphicsBase::Base().AddNextStructure_PhysicalDeviceFeatures(physicalDeviceDynamicRenderingFeatures);
+        if (!InitializeWindow({ 1280, 720 }) ||
+            !physicalDeviceDynamicRenderingFeatures.dynamicRendering)
+            return -1;
+        vkCmdBeginRendering = reinterpret_cast<PFN_vkCmdBeginRenderingKHR>(vkGetDeviceProcAddr(graphicsBase::Base().Device(), "vkCmdBeginRenderingKHR"));
+        vkCmdEndRendering = reinterpret_cast<PFN_vkCmdEndRenderingKHR>(vkGetDeviceProcAddr(graphicsBase::Base().Device(), "vkCmdEndRenderingKHR"));
+    }
+    else
+        if (!InitializeWindow({ 1920,1080 }) ||
+            !graphicsBase::Base().PhysicalDeviceVulkan13Features().dynamicRendering) {
+                std::cerr << "Fatal:Initialize Window failed..." << std::endl;
+                return -1;
+        }
+
 
     // 2d纹理贴图
     texture2d t((rootDir / "source/img.png").c_str(),VK_FORMAT_R8G8B8A8_UNORM,VK_FORMAT_R8G8B8A8_UNORM, true);
@@ -168,17 +216,61 @@ int main() {
         auto i = graphicsBase::Base().CurrentImageIndex();
 
         commandBuffer_graphics.Begin(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
-        graphicsBase::Base().CmdTransferImageOwnership(commandBuffer_graphics);
-        renderPass.CmdBegin(commandBuffer_graphics, framebuffers[i], {{}, windowSize }, clearColor);
+        VkImageMemoryBarrier imageMemoryBarrier = {
+            .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+            .srcAccessMask = 0,
+            .dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+            .oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+            .newLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .image = graphicsBase::Base().SwapchainImage(i),
+            .subresourceRange = { VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1 }
+        };
+        vkCmdPipelineBarrier(
+            commandBuffer_graphics,
+            VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
+            VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+            VK_DEPENDENCY_BY_REGION_BIT,
+            0, nullptr,
+            0, nullptr,
+            1, &imageMemoryBarrier);
 
         // 渲染命令部分
+        VkRenderingAttachmentInfo colorAttachmentInfo = {
+            .sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO,
+            .imageView = graphicsBase::Base().SwapchainImageView(i),
+            .imageLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+            .loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+            .storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+            .clearValue = clearColor
+        };
+        VkRenderingInfo renderingInfo = {
+            .sType = VK_STRUCTURE_TYPE_RENDERING_INFO,
+            .renderArea = { {}, windowSize },
+            .layerCount = 1,
+            .colorAttachmentCount = 1,
+            .pColorAttachments = &colorAttachmentInfo
+        };
+        vkCmdBeginRendering(commandBuffer_graphics, &renderingInfo);
         VkDeviceSize offset = 0;
         vkCmdBindVertexBuffers(commandBuffer_graphics, 0, 1, vertexBuffer.Address(), &offset);
         vkCmdBindPipeline(commandBuffer_graphics, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline_texture);
         vkCmdBindDescriptorSets(commandBuffer_graphics, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout_texture, 0, 1, descriptorSet_texture.Address(), 0, nullptr);
         vkCmdDraw(commandBuffer_graphics, 4, 1, 0, 0);
 
-        renderPass.CmdEnd(commandBuffer_graphics);
+        imageMemoryBarrier.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+        imageMemoryBarrier.dstAccessMask = 0;
+        imageMemoryBarrier.oldLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+        imageMemoryBarrier.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+        vkCmdPipelineBarrier(
+            commandBuffer_graphics,
+            VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+            VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+            VK_DEPENDENCY_BY_REGION_BIT,
+            0, nullptr,
+            0, nullptr,
+            1, &imageMemoryBarrier);
         commandBuffer_graphics.End();
 
         graphicsBase::Base().SubmitCommandBuffer_Graphics(commandBuffer_graphics, semaphore_imageIsAvailable);
